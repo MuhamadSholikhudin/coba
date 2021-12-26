fce2a1e06dfa4fd5bf337a4a0d3ae6bad90aef29
MuhamadSholikhudin <41666877+MuhamadSholikhudin@users.noreply.github.com>
2021-12-26 13:48:57 +0700

    Update post.blade.php

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,6 +3,8 @@
 @section('container')
 
   <article>
+
+<p> By <a href="/categories/{{ $post->category->slug }}"> {{ $post->category->name }}</a> </p>
       <h2>Judul : {{ $blog->title }}</h2>
       <a href="/blog/ {{ $blog->id }}"> {{ $blog->id }} </a>
       <h5>Penulis : {!! $blog['penulis'] !!}</h5>
