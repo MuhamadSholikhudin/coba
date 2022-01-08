762cb6a96a2a4e6f49fc969ebf4a8d1367b11de2
MuhamadSholikhudin <41666877+MuhamadSholikhudin@users.noreply.github.com>
2022-01-08 20:55:59 +0700

    Update DashboardPostController.php

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -90,6 +90,12 @@ class DashboardPostController extends Controller
     public function edit(Post $post)
     {
         //
+        return view('dashboard.posts.edit'
+        , [
+            'post' => $post,
+             'categories' => Category::all()
+        ]
+    );
     }
 
     /**
@@ -102,6 +108,24 @@ class DashboardPostController extends Controller
     public function update(Request $request, Post $post)
     {
         //
+        $rules =  $request->validate([
+            'title' => 'required|max:255',
+            'category_id' => 'required',
+            'body' => 'required'
+        ]);
+
+if($request->slug != $post->slug){
+  $rules['slug'] = 'required|unique:posts';
+}
+
+        $validatedData['user_id'] = auth()->user()->id;
+        $validatedData['excerpt'] = Str::limit(($request->body), 200);
+
+        $validatedData = $request->validate($rules);
+
+        Post::where->('id', '$post->id')
+              ->update($validatedData);
+        return redirect('/dashboard/posts')->with('success', 'Post has been updated!');
     }
 
     /**
@@ -113,6 +137,8 @@ class DashboardPostController extends Controller
     public function destroy(Post $post)
     {
         //
+        Post:: destroy ($post->id);
+        return redirect('/dashboard/posts')->with('success', 'Post has been deleted!');
     }
 
     public function checkSlug(Request $request)
